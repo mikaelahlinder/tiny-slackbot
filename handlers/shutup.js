ae030aca672a76a47e320c7851b836b8d10eeab8
Mikael Ahlinder <mikael.ahlinder@aptitud.se>
2017-03-24 09:36:59 +0100

    sune

--- handlers/shutup.js
+++ handlers/shutup.js
@@ -2,10 +2,10 @@ module.exports = tiny => {
   tiny.listen(/.*/i, (send, match) => {
     send(`Du sa ${match[0]}`)
   })
-  tiny.listen(/tobias/i, send => {
+  tiny.listen(/sune/i, send => {
     send('Jaaaaaaaaa')
   })
-  tiny.speak(/team-customer-dev/i, send => {
-    send('Tobias här!')
+  tiny.speak(/aptitud/i, send => {
+    send('Sune här!')
   })
 }
